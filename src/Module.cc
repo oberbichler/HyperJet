6c32aa245e57c3ab9557a6a5eb8698421eccd309
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-01-13 10:35:43 +0100

    Add Python module

---
+++ src/Module.cc
@@ -0,0 +1,78 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
+#include <HyperJet.h>
+
+PYBIND11_MODULE(HyperJet, m) {
+    m.doc() = "HyperJet by Thomas Oberbichler";
+    m.attr("__author__") = "Thomas Oberbichler";
+    m.attr("__copyright__") = "Copyright (c) 2019, Thomas Oberbichler";
+    m.attr("__version__") = HYPERJET_VERSION;
+    m.attr("__email__") = "thomas.oberbichler@gmail.com";
+    m.attr("__status__") = "Development";
+
+    namespace py = pybind11;
+
+    using Type = HyperJet<double>;
+
+    py::class_<Type>(m, "HyperJet")
+        .def(py::init<int>())
+        .def(py::init<double, Type::Vector>())
+        .def(py::init<double, Type::Vector, Type::Matrix>())
+        .def_property("v", &Type::v, [](Type& self, double value) {
+            self.v() = value;})
+        .def_property_readonly("g", &Type::g)
+        .def_property_readonly("j", &Type::j)
+        .def(-py::self)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def(py::self < py::self)
+        .def(py::self > py::self)
+        .def(py::self <= py::self)
+        .def(py::self >= py::self)
+        .def(py::self == double())
+        .def(py::self != double())
+        .def(py::self < double())
+        .def(py::self > double())
+        .def(py::self <= double())
+        .def(py::self >= double())
+        .def(double() == py::self)
+        .def(double() != py::self)
+        .def(double() < py::self)
+        .def(double() > py::self)
+        .def(double() <= py::self)
+        .def(double() >= py::self)
+        .def(py::self + py::self)
+        .def(py::self + double())
+        .def(py::self - py::self)
+        .def(py::self - double())
+        .def(py::self * py::self)
+        .def(py::self * double())
+        .def(py::self / py::self)
+        .def(py::self / double())
+        .def(py::self += py::self)
+        .def(py::self -= py::self)
+        .def(py::self *= py::self)
+        .def(py::self *= double())
+        .def(py::self /= py::self)
+        .def(py::self /= double())
+        .def(double() + py::self)
+        .def(double() - py::self)
+        .def(double() * py::self)
+        .def(double() / py::self)
+        .def("__repr__", &Type::toString)
+        .def("sqrt", &Type::sqrt)
+        .def("cos", &Type::cos)
+        .def("sin", &Type::sin)
+        .def("tan", &Type::tan)
+        .def("acos", &Type::acos)
+        .def("asin", &Type::asin)
+        .def("atan", &Type::atan)
+        .def_static("atan2", &Type::atan2)
+    ;
+}
