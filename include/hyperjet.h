4d532ec259c9172df8fa70c096dd509ce9aedb76
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2021-07-12 18:05:32 +0200

    Initialize data

--- include/hyperjet.h
+++ include/hyperjet.h
@@ -238,6 +238,7 @@ public:
             static_assert(TSize >= 0);
         }
         m_data[0] = f;
+        std::fill(m_data.begin() + 1, m_data.end(), 0);
     }
 
     DDScalar(const Data& data)
