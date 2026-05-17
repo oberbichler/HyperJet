9925ff2d4a6c9fcebcb9a4d6e37b9b5fe0ca4085
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2026-05-17 10:29:07 +0200

    Add auto formatting (#69)
    * build: migrate to pyproject.toml with scikit-build-core and uv
    
    * fix: cmake version
    
    * fix: classifiers
    
    * fix: artefact naming collision
    
    * fix: test all python versions
    
    * fix: test release build
    
    * fix: redundant env variables
    
    * fix: add concurrency-groups
    
    * fix: remove push-trigger from wheels
    
    * fix: Wheels-CI
    
    * fix: include arm
    
    * add clang format
    
    * switch to LLVM standard
    
    * add ruff auto format

--- python/tests/test_SScalar.py
+++ python/tests/test_SScalar.py
@@ -4,10 +4,11 @@ import numpy as np
 from numpy.testing import assert_equal, assert_allclose
 from copy import copy
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     import sys
-    print(f'pid: {os.getpid()}')
+
+    print(f"pid: {os.getpid()}")
     pytest.main(sys.argv)
 
 
@@ -18,24 +19,24 @@ def test_init():
     u = hj.SScalar(f=1.2)
     assert_equal(len(u), 0)
     assert_allclose(u.f, 1.2)
-    assert_allclose(u.d('x'), 0)
-    assert_allclose(u.d('y'), 0)
-    assert_allclose(u.d('z'), 0)
-    
-    u = hj.SScalar(f=1.2, d={'x': 1, 'y': 2})
+    assert_allclose(u.d("x"), 0)
+    assert_allclose(u.d("y"), 0)
+    assert_allclose(u.d("z"), 0)
+
+    u = hj.SScalar(f=1.2, d={"x": 1, "y": 2})
     assert_equal(len(u), 2)
     assert_allclose(u.f, 1.2)
-    assert_allclose(u.d('x'), 1)
-    assert_allclose(u.d('y'), 2)
-    assert_allclose(u.d('z'), 0)
+    assert_allclose(u.d("x"), 1)
+    assert_allclose(u.d("y"), 2)
+    assert_allclose(u.d("z"), 0)
 
 
 def test_mul():
-    u = hj.SScalar(f=1, d={'x': 1})
-    v = hj.SScalar(f=1, d={'y': 1})
+    u = hj.SScalar(f=1, d={"x": 1})
+    v = hj.SScalar(f=1, d={"y": 1})
     r = u + v**2
     assert_equal(len(r), 2)
     assert_allclose(r.f, 2)
-    assert_allclose(r.d('x'), 1)
-    assert_allclose(r.d('y'), 2)
-    assert_allclose(r.d('z'), 0)
+    assert_allclose(r.d("x"), 1)
+    assert_allclose(r.d("y"), 2)
+    assert_allclose(r.d("z"), 0)
